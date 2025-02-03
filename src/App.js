9c36c03621f679db96939b1e1cf42bade71fc19e
OtaiLotfi <127872430+OtaiLotfi@users.noreply.github.com>
2025-02-03 20:34:47 +0000

    Changed a link title.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          OTAI DevOps Jenkins
         </a>
       </header>
       <p>
