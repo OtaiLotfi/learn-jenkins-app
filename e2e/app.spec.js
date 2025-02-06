0daf846c14382ac9b68d89dc5dd4f38599d2eb8d
OtaiLotfi <127872430+OtaiLotfi@users.noreply.github.com>
2025-02-06 23:22:49 +0000

    Fix an issue.

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/OTAI DevOps/);
+  await expect(page).toHaveTitle(/OTAI DevOps/, { timeout: 10000 });
 });
 
 test('has Jenkins in the body', async ({ page }) => {
