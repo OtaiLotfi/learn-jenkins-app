f85d9161ca7297de94d84f189e8c04ba9b4af7b6
OtaiLotfi <127872430+OtaiLotfi@users.noreply.github.com>
2025-02-06 23:12:57 +0000

    Fixed an issue.

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -4,7 +4,6 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-   Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/OTAI DevOps/);
 });
 
