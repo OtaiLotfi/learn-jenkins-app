e664b41af9ff837ecc0a8c75bea48231745b4b46
OtaiLotfi <127872430+OtaiLotfi@users.noreply.github.com>
2025-02-07 01:57:56 +0000

    Fixed an issue.

--- playwright.config.js
+++ playwright.config.js
@@ -23,8 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit'],
-    ['allure-playwright', { outputFolder: 'allure-results' }]
+    ['junit']
     ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
